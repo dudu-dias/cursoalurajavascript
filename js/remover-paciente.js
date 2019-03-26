910805a9cb8dbd1600811ded82098bc455900744
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2019-03-25 21:37:28 -0300

    Aula 7 final

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,7 +3,11 @@ var paciente = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick",function(event){
-    event.target.parentNode.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+      event.target.parentNode.remove();
+    },500);
+
 });
 
 //paciente.forEach(function(paciente){
